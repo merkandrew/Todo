406585677361412976fbbbadb915137c06fd44ba
merkandrew <58116052+merkandrew@users.noreply.github.com>
2023-08-15 23:48:45 +0200

    Setup json-server and React Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import AddTodo from "./pages/AddTodo";
 import EditTodo from "./pages/EditTodo";
+
+const client = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -22,6 +25,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={client}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
   </React.StrictMode>
 );
